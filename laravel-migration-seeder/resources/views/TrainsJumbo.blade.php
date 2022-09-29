836f5bdf7496b10ba1f54a8314060c30c679f017
AlessioMaria5 <alessiomaria5@gmail.com>
2022-09-29 17:39:31 +0200

    stampa treni fase 1

---
+++ laravel-migration-seeder/resources/views/TrainsJumbo.blade.php
@@ -0,0 +1,24 @@
+<div class="myTrainRow">
+    @foreach ($trains as $train)
+    <span>{{$train->azienda}}</span> 
+    -
+    <span>{{$train->stazione_partenza}}</span> 
+    -
+    <span>{{$train->stazione_arrivo}}</span> 
+    -
+    <span>{{$train->orario_partenza}}</span> 
+    -
+    <span>{{$train->orario_arrivo}}</span> 
+    -
+    <span>{{$train->codice_treno}}</span> 
+    -
+    <span>{{$train->numero_carrozze}}</span> 
+    -
+    <span>{{$train->in_orario}}</span> 
+    -
+    <span>{{$train->cancellato}}</span> 
+    -
+    <br>
+    @endforeach
+
+</div>
