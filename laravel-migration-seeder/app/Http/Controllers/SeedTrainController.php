ec5efa3925b9ccc52a70b46621fab0d7fa92b4da
AlessioMaria5 <alessiomaria5@gmail.com>
2022-09-30 10:32:01 +0200

    aggiunta bg

--- laravel-migration-seeder/app/Http/Controllers/SeedTrainController.php
+++ laravel-migration-seeder/app/Http/Controllers/SeedTrainController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\SeedTrain;
 use App\Train;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class SeedTrainController extends Controller
 {
     public function index(){
 
         $mytrains = SeedTrain::all();
+        
 
         return view('TrainsJumbo', compact('mytrains')); 
     }
